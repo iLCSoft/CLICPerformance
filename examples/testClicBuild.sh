83ae9d6a943ad08137e4f6c1de57411f78e444e0
Daniel Hynds <daniel.hynds@cern.ch>
2015-06-26 15:07:45 +0000

    updated paths to run on any install location

--- examples/testClicBuild.sh
+++ examples/testClicBuild.sh
@@ -3,28 +3,28 @@
 # check if the detector model has been specified
 ARGS=1
 if [ $# -ne "$ARGS" ]
-then
-  echo "Please give the detector model to run over"
+then 
+  echo -e "\nPlease give the detector model to run over, eg. CLIC_o2_v01\n"
   exit
 fi
 
 # set up the environment
-unset MARLIN_DLL
-source ~/clic/setup.sh
+export MARLIN_DLL=${MARLIN_DLL}../lib/libClicPerformance.so:
 export PYTHONPATH=${LCIO}/src/python:${ROOTSYS}/lib
 source $DD4HEP/bin/thisdd4hep.sh
  
 # find the detector model specified
 DETECTOR="$(find $ILCSOFT/lcgeo -name ${1}.xml)" 
-echo Detector is $DETECTOR
+echo -e "\nPicking up detector from: $DETECTOR"
 
 # generate 1000 muons in random directions
-#python lcio_particle_gun.py
+echo "Generating particles"
+python lcio_particle_gun.py &> particle.log
 
 # run geant4 with the detector model specified
-#python lcio_geant4.py $DETECTOR
+echo "Running GEANT4"
+python lcio_geant4.py $DETECTOR &> geant4.log
 
 # run the CLIC reconstruction chain includin all tracking detectors
-#echo $MARLIN_DLL
-Marlin clicReconstruction.xml
+Marlin clicReconstruction.xml 
 
