b332f9d9757ed30fed2e86105687e8c2581f438a
Rosa Simoniello <Rosa.Simoniello@cern.ch>
2015-07-24 14:22:31 +0000

    added Linkdef.h for vector variables in ntuple

---
+++ Tracking/include/LinkDef.h
@@ -0,0 +1,18 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all defined_ines;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class std::vector< std::vector<double> >+;
+#pragma link C++ class std::vector< double >+;
+#pragma link C++ class std::vector< std::vector<int> >+;
+#pragma link C++ class std::vector< int >+;
+
+//#pragma link C++ class std::vector< std::vector<unsigned int> >+;
+//#pragma link C++ class std::vector< std::map< const std::string, int > >+;
+//#pragma link C++ class std::vector< std::map< const std::string, unsigned > >+;
+//#pragma link C++ class std::vector< std::map< const std::string, float > >+;
+
+#endif
