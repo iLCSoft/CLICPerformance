f1502c84d2c07bdab612331361fc28e91aee8dd6
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-05-26 18:36:33 +0200

    Add CLICRecoConfig processor to chose which configuration to use for reconstruction: truth conformal tracking, if and which overlay

---
+++ source/Conditions/include/CLICRecoConfig.h
@@ -0,0 +1,64 @@
+#ifndef CLICRecoConfig_h
+#define CLICRecoConfig_h 1
+
+#include "marlin/Processor.h"
+#include "marlin/EventModifier.h"
+
+class CLICRecoConfig : public marlin::Processor, public marlin::EventModifier {
+
+public:
+
+  virtual marlin::Processor*  newProcessor() { return new CLICRecoConfig; }
+  virtual std::string const& name() const { return marlin::Processor::name(); };
+
+  CLICRecoConfig() ;
+  CLICRecoConfig(const CLICRecoConfig&) = delete;
+  CLICRecoConfig& operator=(const CLICRecoConfig&) = delete;
+
+  // Initialisation - run at the beginning to start histograms, etc.
+  virtual void init() ;
+
+  // Called at the beginning of every run
+  virtual void processRunHeader( LCRunHeader* run ) ;
+
+  // Run over each event - the main algorithm
+  virtual void modifyEvent( LCEvent* evt ) ;
+
+  // Run at the end of each event
+  virtual void check( LCEvent* evt ) ;
+
+  // Called at the very end for cleanup, histogram saving, etc.
+  virtual void end() ;
+
+protected:
+
+  // Parameters
+  const std::vector< std::string > m_trackingPossibleOptions{ "Truth", "Conformal" };
+  std::string m_trackingOption="Truth";
+
+  // Parameters
+  std::vector< std::string > m_overlayPossibleOptions{ "False", "3TeV", "380GeV" };
+  std::map<std::string, bool> m_overlay{};
+  std::string m_overlayChoice="False";
+
+  bool m_truthTracking = true;
+  bool m_conformalTracking = false;
+
+
+protected:
+  void checkOptions( std::string const& optionValue, std::vector<std::string> const& options ) const;
+
+  template< class T > static void toString( std::stringstream& stream, std::vector<T> const& vector) {
+    unsigned int counter = 0;
+    for (auto const& entry: vector) {
+      stream << entry;
+      if( counter < vector.size() - 1 ) {
+        stream << ", ";
+      }
+      counter++;
+    }
+  }
+
+} ;
+
+#endif
---
+++ source/Conditions/src/CLICRecoConfig.cc
@@ -0,0 +1,108 @@
+#include "CLICRecoConfig.h"
+
+#include <iomanip>
+
+
+CLICRecoConfig aCLICRecoConfig;
+
+CLICRecoConfig::CLICRecoConfig() : Processor("CLICRecoConfig") {
+
+  // modify processor description
+  _description = "CLICRecoConfig allows one to configure the processors to run via command line" ;
+
+  std::stringstream trackingOptionDescription;
+  trackingOptionDescription << "Which option to use for tracking: ";
+  toString( trackingOptionDescription, m_trackingPossibleOptions );
+
+  registerProcessorParameter("Tracking",
+                             trackingOptionDescription.str(),
+                             m_trackingOption,
+                             m_trackingOption);
+
+
+  std::stringstream overlayOptionDescription;
+  overlayOptionDescription << "Which option to use for overlay: ";
+  toString( overlayOptionDescription, m_overlayPossibleOptions );
+  overlayOptionDescription <<". Then use, e.g., Config.Overlay3TeV in the condition";
+  registerProcessorParameter("Overlay",
+                             overlayOptionDescription.str(),
+                             m_overlayChoice,
+                             m_overlayChoice);
+
+}
+
+
+void CLICRecoConfig::init() {
+
+  // Print the initial parameters
+  printParameters() ;
+
+  checkOptions( m_trackingOption, m_trackingPossibleOptions );
+
+  if( m_trackingOption == "Truth" ) {
+
+    m_truthTracking = true;
+    m_conformalTracking = false;
+
+  } else if( m_trackingOption == "Conformal" ) {
+
+    m_truthTracking = false;
+    m_conformalTracking = true;
+
+  }
+
+
+
+  // Treat option for overlay
+  checkOptions( m_overlayChoice, m_overlayPossibleOptions );
+
+  //fill map with overlay options
+  for ( auto& overlayEnergy: m_overlayPossibleOptions ) {
+    if( overlayEnergy == "False" ){ continue; }
+    m_overlay["Overlay"+ overlayEnergy] = (overlayEnergy == m_overlayChoice);
+  }
+
+  for ( auto& overlayEnergy: m_overlay ) {
+    streamlog_out(MESSAGE) << std::setw(15) << overlayEnergy.first << " "
+                           << std::boolalpha << overlayEnergy.second  << std::endl;
+  }
+
+}
+
+void CLICRecoConfig::processRunHeader( LCRunHeader* ){}
+
+void CLICRecoConfig::modifyEvent( LCEvent* ) {
+  streamlog_out(MESSAGE) << "Running Config" << std::endl;
+
+  setReturnValue( "TruthTracking", m_truthTracking );
+  setReturnValue( "ConformalTracking", m_conformalTracking );
+
+  for (auto const& over : m_overlay) {
+    setReturnValue( over.first, over.second );
+  }
+
+}
+
+void CLICRecoConfig::check( LCEvent* ){}
+
+
+void CLICRecoConfig::end(){}
+
+void CLICRecoConfig::checkOptions( std::string const& optionValue, std::vector<std::string> const& options ) const {
+
+  bool validOption = false;
+  for(auto const& opt : options) {
+    if( optionValue == opt) {
+      validOption = true;
+      break;
+    }
+  }
+  if( not validOption ){
+    std::stringstream errorMessage;
+    errorMessage << "Invalid option value '" << optionValue << "'. Use one of [";
+    toString( errorMessage, options );
+    errorMessage << "]!";
+    throw std::runtime_error( errorMessage.str() );
+  }
+
+}
