8c38d21dddb6c01d15ce7d3cc586bb7761106569
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-12-05 16:44:40 +0100

    Tests: add commands to run profiler on Reconstruction

---
+++ Tests/bin/Profiler.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Simple script to run marlin tests with profiler on top
+#   calls the command (given as second argument)
+#   with all following arguments
+
+#----- initialize profiler environment
+source /cvmfs/projects.cern.ch/intelsw/psxe/linux/all-setup.sh
+#----- parse command line - first argument is the
+#      test to run
+## Drop existing ClicPerformance from MARLIN_DLL
+MARLIN_DLL=`echo $MARLIN_DLL | sed -e 's/:\?[-a-z0-9A-Z_/.]*libClicPerformance.so[.0-9]*:\?/:/'`
+export MARLIN_DLL=$1:$MARLIN_DLL
+command=$2
+theargs=""
+
+##Drop two
+shift
+shift
+for i in "$@" ; do
+    theargs="${theargs} $i"
+done
+
+echo " #### MARLIN_DLL = :  ${MARLIN_DLL}"
+
+echo " ### running test :  '${command} ${theargs}'"
+exec ${command} ${theargs}
