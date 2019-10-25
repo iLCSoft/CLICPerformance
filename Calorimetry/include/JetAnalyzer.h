792985fc4b833e0cc7d3c4dcaa3f300996423185
Matthias Artur Weber <matthias.artur.weber@cern.ch>
2019-10-25 11:23:18 +0200

    renaming to clarify that All is actually the sum of all true visible particles, comments on branch content in class header file

--- Calorimetry/include/JetAnalyzer.h
+++ Calorimetry/include/JetAnalyzer.h
@@ -64,19 +64,20 @@ protected:
   // Run and event counters
   int m_eventNumber=0;
   int m_runNumber=0;
-  //parton level information, save two Z daughters
+  //parton level information, save first quark in history
   int m_d1_mcPDGID=0;
   float m_d1_mcE=0;
   float m_d1_mcPx=0;
   float m_d1_mcPy=0;
   float m_d1_mcPz=0;
-
+  //parton level information, save second quark in history
   int m_d2_mcPDGID=0;
   float m_d2_mcE=0;
   float m_d2_mcPx=0;
   float m_d2_mcPy=0;
   float m_d2_mcPz=0;
   //particle level information sums --> visible and invisible energy
+
   //only neutrinos
   float m_E_trueInv=0;
   float m_px_trueInv=0;
@@ -84,10 +85,10 @@ protected:
   float m_pz_trueInv=0;
 
   //all visible particles
-  float m_E_trueAll=0;
-  float m_px_trueAll=0;
-  float m_py_trueAll=0;
-  float m_pz_trueAll=0;
+  float m_E_trueVis=0;
+  float m_px_trueVis=0;
+  float m_py_trueVis=0;
+  float m_pz_trueVis=0;
 
   //all reconstructed particles
   float m_E_totPFO=0;
@@ -95,6 +96,7 @@ protected:
   float m_py_totPFO=0;
   float m_pz_totPFO=0;
 
+  //if extended parton quantities are saved: dibosons and their decay products
   std::vector<float>* m_trueME_E=NULL;
   std::vector<float>* m_trueME_Px=NULL;
   std::vector<float>* m_trueME_Py=NULL;
